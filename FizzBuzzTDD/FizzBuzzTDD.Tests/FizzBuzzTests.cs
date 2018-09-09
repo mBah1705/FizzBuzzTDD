7d0e296d1f3b9baf3664efa2ddb3b8a581d0b8bf
Mamadou Bah <m.bah1705@gmail.com>
2018-09-09 16:41:06 +0200

    Created project and First Test (Is in range)

---
+++ FizzBuzzTDD/FizzBuzzTDD.Tests/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTDD.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void When20_ShouldReturn20_IsInRange()
+        {
+            FizzBuzz Buzzer = new FizzBuzz();
+            Assert.AreEqual(Buzzer.IsInRange(20), true);
+        }
+    }
+}
---
+++ FizzBuzzTDD/FizzBuzzTDD/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FizzBuzzTDD
+{
+    public class FizzBuzz
+    {
+        private int min = 1;
+        private int max = 100;
+
+        public bool IsInRange(int i) => min <= i && i <= max;
+    }
+
+
+}
