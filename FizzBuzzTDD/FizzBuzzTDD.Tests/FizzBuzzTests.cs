da5c58bede7c7a97d8c4d76c6aade9ba6365fb4d
Mamadou Bah <m.bah1705@gmail.com>
2018-09-09 17:31:15 +0200

    Added IsFizz method and tested it

--- FizzBuzzTDD/FizzBuzzTDD.Tests/FizzBuzzTests.cs
+++ FizzBuzzTDD/FizzBuzzTDD.Tests/FizzBuzzTests.cs
@@ -7,10 +7,32 @@ namespace FizzBuzzTDD.Tests
     public class FizzBuzzTests
     {
         [TestMethod]
-        public void When20_ShouldReturn20_IsInRange()
+        public void When_20_ShouldReturn20_IsInRange()
         {
             FizzBuzz Buzzer = new FizzBuzz();
             Assert.AreEqual(Buzzer.IsInRange(20), true);
+        }
+
+
+        [TestMethod]
+        public void When_0_Should_NotReturnFizz_BecauseOutOfRange()
+        {
+            FizzBuzz Buzzer = new FizzBuzz();
+            Assert.AreEqual(Buzzer.IsFizz(1), false);
+        }
+
+        [TestMethod]
+        public void When_1_Should_NotReturnFizz()
+        {
+            FizzBuzz Buzzer = new FizzBuzz();
+            Assert.AreEqual(Buzzer.IsFizz(1), false);
+        }
+
+        [TestMethod]
+        public void When_6_Should_ReturnFizz()
+        {
+            FizzBuzz Buzzer = new FizzBuzz();
+            Assert.AreEqual(Buzzer.IsFizz(6), true);
         }
     }
 }
--- FizzBuzzTDD/FizzBuzzTDD/FizzBuzz.cs
+++ FizzBuzzTDD/FizzBuzzTDD/FizzBuzz.cs
@@ -11,6 +11,18 @@ namespace FizzBuzzTDD
         private int max = 100;
 
         public bool IsInRange(int i) => min <= i && i <= max;
+
+        public bool IsFizz(int i)
+        {
+            if (IsInRange(i))
+            {
+                return i % 3 == 0;
+            }
+
+            Console.WriteLine($"{i} is out of range!");
+            return false;
+            
+        }
     }
 
 
